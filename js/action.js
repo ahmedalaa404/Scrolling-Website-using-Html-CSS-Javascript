0bf058887a14d2ac06f9d0d68c0c707957b2903e
ahmed alaa yassin <az.ahmed.alaa@gmail.com>
2022-10-10 03:33:44 +0200

    upload files of html and css and call element of html in the file of javascript

---
+++ js/action.js
@@ -0,0 +1,12 @@
+let stars = document.getElementById("stars");
+let mountain3 = document.getElementById("mountain3");
+
+let moon = document.getElementById("moon");
+
+let nouvile = document.getElementById("nouvile");
+
+let mountain4 = document.getElementById("mountain4");
+
+let river = document.getElementById("river");
+
+let boat = document.getElementById("boat");
